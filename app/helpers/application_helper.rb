c9949638ce3ca89c0a95addf5a40a2b83358bd46
Le Tat Tung <letattungtb@gmail.com>
2017-05-10 15:03:59 +0700

    reply comment

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,5 +6,21 @@ module ApplicationHelper
   def logged_in?
     current_user.present?
   end
-  
+
+  def avatar_profile user
+    if user && user.avatar
+      user.avatar
+    else
+      "#{user.sex}.png"
+    end
+  end
+
+  def user_reply_comment comment
+    if comment
+      comment_reply = comment.reply
+      if comment_reply && comment_reply.user != comment.user
+        link_to comment_reply.user.name, comment_reply.user
+      end
+    end
+  end
 end
--- db/migrate/20170413033954_create_users.rb
+++ db/migrate/20170413033954_create_users.rb
@@ -3,7 +3,8 @@ class CreateUsers < ActiveRecord::Migration[5.0]
     create_table :users do |t|
       t.string :name
       t.string :avatar
-      t.boolean :admin
+      t.integer :sex, default: 0
+      t.boolean :admin, default: false
 
       t.timestamps
     end
